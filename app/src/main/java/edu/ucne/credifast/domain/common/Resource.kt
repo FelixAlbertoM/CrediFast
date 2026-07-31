bf96c22749354feb11f886ee54865f87b91883f3
FelixAlbertoM <af301509@gmail.com>
2026-07-30 22:37:47 -0400

    Crea Resource.kt

---
+++ app/src/main/java/edu/ucne/credifast/domain/common/Resource.kt
@@ -0,0 +1,7 @@
+package edu.ucne.credifast.domain.common
+
+sealed class Resource<out T> {
+    data class Success<out T>(val data: T) : Resource<T>()
+    data class Error(val message: String) : Resource<Nothing>()
+    data object Loading : Resource<Nothing>()
+}
