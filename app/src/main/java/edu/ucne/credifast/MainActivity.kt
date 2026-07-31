ca5c366d2cd0c148c3914947f625c523523d9642
FelixAlbertoM <af301509@gmail.com>
2026-07-30 22:50:00 -0400

    modifica archivos para duncionamiento optimo

--- app/src/main/java/edu/ucne/credifast/MainActivity.kt
+++ app/src/main/java/edu/ucne/credifast/MainActivity.kt
@@ -5,43 +5,31 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.credifast.domain.auth.usecase.GetCurrentUserUseCase
+import edu.ucne.credifast.presentation.navigation.CrediFastNavHost
 import edu.ucne.credifast.ui.theme.CrediFastTheme
-
+import javax.inject.Inject
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var getCurrentUserUseCase: GetCurrentUserUseCase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val isLoggedIn = getCurrentUserUseCase.isLoggedIn()
+
         setContent {
             CrediFastTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    CrediFastNavHost(isLoggedIn = isLoggedIn)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CrediFastTheme {
-        Greeting("Android")
-    }
-}
