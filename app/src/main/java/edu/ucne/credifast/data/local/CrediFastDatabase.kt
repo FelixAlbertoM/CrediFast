86c648d67db5944a92f094044ff624576a1c85ab
RomelOrtega <romel_ortega@ucne.edu.do>
2026-08-01 09:02:15 -0400

    Agrega Prestamo y Cuota a la Db

--- app/src/main/java/edu/ucne/credifast/data/local/CrediFastDatabase.kt
+++ app/src/main/java/edu/ucne/credifast/data/local/CrediFastDatabase.kt
@@ -3,19 +3,28 @@ package edu.ucne.credifast.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.credifast.data.local.dao.ClienteDao
-import edu.ucne.credifast.data.local.entities.ClienteEntity
+import edu.ucne.credifast.data.local.dao.CuotaDao
+import edu.ucne.credifast.data.local.dao.PrestamoDao
 import edu.ucne.credifast.data.local.dao.UsuarioDao
+import edu.ucne.credifast.data.local.entities.ClienteEntity
+import edu.ucne.credifast.data.local.entities.CuotaEntity
+import edu.ucne.credifast.data.local.entities.PrestamoEntity
 import edu.ucne.credifast.data.local.entities.UsuarioEntity
+
 
 @Database(
     entities = [
         UsuarioEntity::class,
-        ClienteEntity::class
+        ClienteEntity::class,
+        PrestamoEntity::class,
+        CuotaEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class CrediFastDatabase : RoomDatabase() {
     abstract fun usuarioDao(): UsuarioDao
     abstract fun clienteDao(): ClienteDao
+    abstract fun prestamoDao(): PrestamoDao
+    abstract fun cuotaDao(): CuotaDao
 }
