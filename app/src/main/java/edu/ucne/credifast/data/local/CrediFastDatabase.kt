f9fd30950cf102ca5366193eef98643f91344722
RomelOrtega <romel_ortega@ucne.edu.do>
2026-08-03 16:55:29 -0400

    Actualiza la DataBase

--- app/src/main/java/edu/ucne/credifast/data/local/CrediFastDatabase.kt
+++ app/src/main/java/edu/ucne/credifast/data/local/CrediFastDatabase.kt
@@ -4,22 +4,24 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.credifast.data.local.dao.ClienteDao
 import edu.ucne.credifast.data.local.dao.CuotaDao
+import edu.ucne.credifast.data.local.dao.PagoDao
 import edu.ucne.credifast.data.local.dao.PrestamoDao
 import edu.ucne.credifast.data.local.dao.UsuarioDao
 import edu.ucne.credifast.data.local.entities.ClienteEntity
 import edu.ucne.credifast.data.local.entities.CuotaEntity
+import edu.ucne.credifast.data.local.entities.PagoEntity
 import edu.ucne.credifast.data.local.entities.PrestamoEntity
 import edu.ucne.credifast.data.local.entities.UsuarioEntity
-
 
 @Database(
     entities = [
         UsuarioEntity::class,
         ClienteEntity::class,
         PrestamoEntity::class,
-        CuotaEntity::class
+        CuotaEntity::class,
+        PagoEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class CrediFastDatabase : RoomDatabase() {
@@ -27,4 +29,5 @@ abstract class CrediFastDatabase : RoomDatabase() {
     abstract fun clienteDao(): ClienteDao
     abstract fun prestamoDao(): PrestamoDao
     abstract fun cuotaDao(): CuotaDao
+    abstract fun pagoDao(): PagoDao
 }
