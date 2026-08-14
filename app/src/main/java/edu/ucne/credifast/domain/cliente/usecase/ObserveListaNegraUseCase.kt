b10547e2fc78b8a36eacf648e6143b597dd02a1c
FelixAlbertoM <af301509@gmail.com>
2026-08-14 12:56:14 -0400

    Crea el caso de uso de ObserveListaNegra

---
+++ app/src/main/java/edu/ucne/credifast/domain/cliente/usecase/ObserveListaNegraUseCase.kt
@@ -0,0 +1,12 @@
+package edu.ucne.credifast.domain.cliente.usecase
+
+import edu.ucne.credifast.domain.cliente.model.Cliente
+import edu.ucne.credifast.domain.cliente.repository.ClienteRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ObserveListaNegraUseCase @Inject constructor(
+    private val repository: ClienteRepository
+) {
+    operator fun invoke(): Flow<List<Cliente>> = repository.observeListaNegra()
+}
